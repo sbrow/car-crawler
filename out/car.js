bbde0547d36a6969c71d149c69726dc9c8c371df
Spencer Brower <brower.spencer@gmail.com>
2019-04-01 13:30:09 -0400

    Removed node_modules

---
+++ out/car.js
@@ -0,0 +1,14 @@
+var Car = /** @class */ (function () {
+    function Car(data) {
+        this.make = data.make;
+        this.model = data.model;
+        this.exterior = data.exterior;
+        this.interior = data.interior;
+        this.vin = data.vin;
+        this.seller = data.seller.name + "\n" + data.seller.address + "\n" + data.seller.phone;
+        this.price = data.price;
+        this.link = data.url;
+    }
+    return Car;
+}());
+//# sourceMappingURL=car.js.map
