471f4e4c6d8519ff282682301f49030d4b6854f2
Spencer Brower <foxtrotalphalima@gmail.com>
2019-04-09 18:00:00 -0400

    sdaf

--- out/package.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// import { main } from "./main";
-// import { sites } from "./sites";
-exports.__esModule = true;
-function main() { }
-exports.main = main;
-require("./main").main;
-exports.sites = require("./sites").sites;
-//# sourceMappingURL=package.js.map
--- src/package.ts
+++
@@ -1,5 +0,0 @@
-// import { main } from "./main";
-// import { sites } from "./sites";
-
-export function main = require("./main").main;
-export const sites = require("./sites").sites;
